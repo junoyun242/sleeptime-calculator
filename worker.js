d3870ee78d1cc056a3c5a0eef1807e615013e4cf
apeiron242 <apeiron242@gmail.com>
2021-04-18 08:49:42 +0900

    Updates

---
+++ worker.js
@@ -0,0 +1,38 @@
+const cacheName = "21/04/18";
+
+// Call Install Event
+self.addEventListener("install", (e) => {});
+
+// Call Activate Event
+self.addEventListener("activate", (e) => {
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call fetch event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // make copy/clone of response
+        const resClone = res.clone();
+        // Open cache
+        caches.open(cacheName).then((cache) => {
+          // Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(e.request).then((res) => res))
+  );
+});
