499bd42583be45b712b816e27f762d888251e74b
apeiron242 <apeiron242@gmail.com>
2021-04-18 09:13:56 +0900

    added reload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-    <header
-      className="border-b-2 border-gray-300 flex flex-col justify-center items-center py-5 bg-gray-300"
-      onClick={() => window.location.reload()}
-    >
-      <h1 className="text-2xl">Sleeptime Calculator</h1>
+    <header className="border-b-2 border-gray-300 flex flex-col justify-center items-center py-5 bg-gray-300">
+      <h1 className="text-2xl" onClick={() => window.location.reload()}>
+        Sleeptime Calculator
+      </h1>
     </header>
   );
 }
